cb646e9a8ac2de5ed3d15909d79ae70aae8cb028
Your Name Comes Here <you@yourdomain.example.com>
2021-06-09 09:25:52 -0700

    add index.py

---
+++ index.py
@@ -0,0 +1,99 @@
+import random
+
+def hangman():
+
+    word = random.choice(["pugger" , "littlepugger" , "tiger" , "superman" , "thor" , "pokemon" , "avengers" , "savewater" , "earth" , "swag"])
+    validLetters = 'abcdefghijklmnopqrstuvwxyz'
+    turns = 10
+    guessmade = ''
+
+    print("Debug - secretWord: " + word)
+
+    while len(word) > 0:
+        main = ""
+        missed = 0
+
+        for letter in word:
+            if letter in guessmade:
+                main = main + letter
+            else:
+                main = main + "_" + " "
+        if main == word:
+            print(main)
+            print("You win!")
+            break
+
+        print("Guess the word:" , main)
+        guess = input()
+
+        if guess in validLetters:
+            guessmade = guessmade + guess
+        else:
+            print("Enter a valid character")
+            guess = input()
+
+        if guess not in word:
+            turns = turns - 1
+            if turns == 9:
+                print("9 turns left")
+                print(" --------- ")
+            if turns == 8:
+                print("8 turns left")
+                print(" --------- ")
+                print("     O     ")
+            if turns == 7:
+                print("7 turns left")
+                print(" --------- ")
+                print("     O     ")
+                print("     |     ")
+            if turns == 6:
+                print("6 turns left")
+                print(" --------- ")
+                print("     O     ")
+                print("     |     ")
+                print("    /      ")
+            if turns == 5:
+                print("5 turns left")
+                print(" --------- ")
+                print("     O     ")
+                print("     |     ")
+                print("    / \     ")
+            if turns == 4:
+                print("4 turns left")
+                print(" --------- ")
+                print("     O     ")
+                print("    -|     ")
+                print("    / \     ")
+            if turns == 3:
+                print("3 turns left")
+                print(" --------- ")
+                print("     O     ")
+                print("    -|-    ")
+                print("    / \     ")
+            if turns == 2:
+                print("2 turns left")
+                print(" --------- ")
+                print("     O |   ")
+                print("    -|-    ")
+                print("    / \     ")
+            if turns == 1:
+                print("1 turns left")
+                print(" --------- ")
+                print("     O_|    ")
+                print("    -|-    ")
+                print("    / \     ")
+            if turns == 0:
+                print("You lose!")
+                break
+
+
+
+
+
+
+
+name = input("Enter your name:")
+print("Welcome" , name )
+print("-------------------------------------------------- \nTry to guess the word in less than 10 attempts")
+hangman()
+print()
